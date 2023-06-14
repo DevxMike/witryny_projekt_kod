5caf1b222d831f221ec53fbbfcfb61c0ac88346c
Mateusz Tomecki <merlishjzkl@gmail.com>
2023-06-14 20:55:57 +0200

    Download fixed

--- html/chartshow.js
+++ html/chartshow.js
@@ -3,7 +3,7 @@
 
 
 
-function get_data_range() {
+function get_data_range(event) {
   event.preventDefault();
   let startDate = document.getElementById('start-date').value;
   let endDate = document.getElementById('end-date').value;
@@ -17,19 +17,39 @@ function get_data_range() {
       console.error(error);
     });
 }
-// Pobierz dane z serwera
+
 
 
 
  function download_chart() {
-
-  let base64Image = massPopChart.toBase64Image();
+let base64Image;
+let chartTypeName;
+  if (massPopChart == null) {
+    alert("No chart to download");
+    return;
+  }
+  if (document.getElementById('temperature').style.display == 'inline-block') {
+     base64Image = massPopChart.toBase64Image();
+     chartTypeName = "temperature";
+  }
+  else if (document.getElementById('pressure').style.display == 'inline-block') {
+     base64Image = massPopChart2.toBase64Image();
+     chartTypeName = "pressure";
+  }
+  else if (document.getElementById('humidity').style.display == 'inline-block') {
+     base64Image = massPopChart1.toBase64Image();
+     chartTypeName = "humidity";
+  }
+  else if (document.getElementById('air_quality').style.display == 'inline-block') {
+     base64Image = massPopChart3.toBase64Image();
+     chartTypeName  = "air_quality";
+  }
 
   let currentDate = new Date();
   let timezoneOffset = currentDate.getTimezoneOffset(); 
   let adjustedDate = new Date(currentDate.getTime() - timezoneOffset);
 
-  let fileName = 'chart_' + adjustedDate.toLocaleString() + '.png';
+  let fileName = 'chart_' + adjustedDate.toLocaleString() + '_' + chartTypeName + '.png';
 
   let downloadLink = document.createElement('a');
   downloadLink.href = base64Image;
@@ -45,7 +65,6 @@ let massPopChart2=null;
 let massPopChart3=null;
 
 function drawChart(data) {
-
     // Wykres temperatury
     let temperature = document.getElementById('temperature').getContext('2d');
     if (Chart.getChart("temperature")){
@@ -179,12 +198,6 @@ function drawChart(data) {
 
     
 // Przełączanie wykresów
-document.getElementById('btn-temperature').addEventListener('click', function() {
-  document.getElementById('temperature').style.display = 'inline-block';
-  document.getElementById('humidity').style.display = 'none';
-  document.getElementById('pressure').style.display = 'none';
-  document.getElementById('air_quality').style.display = 'none';
-});
 
 document.getElementById('btn-humidity').addEventListener('click', function() {
   document.getElementById('temperature').style.display = 'none';
